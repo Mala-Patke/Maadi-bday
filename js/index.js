ea00f1c0a1b6a132d6dfdf1b03e892f76dbedaa1
Mala-Patke <66596286+Mala-Patke@users.noreply.github.com>
2022-05-30 21:05:35 -0700

    bug fix 1

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ window.onload = () => {
 
     for(let button of document.getElementsByTagName('button')) {
         button.addEventListener('click', () => {
-            window.location.href = `/game.htm?code=${button.getAttribute('code')}`
+            window.location.href = `./game.htm?code=${button.getAttribute('code')}`
         });
     }
 };
